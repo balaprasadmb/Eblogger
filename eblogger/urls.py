bc52d960ad9b6f3169d117a931ee2c637d6e1e2b
Balaprasad Borade <balaprasadmborade@gmail.com>
2017-04-25 17:35:21 +0530

    added main app files
    added main app files

---
+++ eblogger/urls.py
@@ -0,0 +1,60 @@
+"""Balas URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
+from eblogger.activities import views as activities_views
+from eblogger.authentication import views as bootcamp_auth_views
+from eblogger.core import views as core_views
+from eblogger.search import views as search_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', core_views.home, name='home'),
+    url(r'^login', auth_views.login, {'template_name': 'core/cover.html'},
+        name='login'),
+    url(r'^logout', auth_views.logout, {'next_page': '/'}, name='logout'),
+    url(r'^signup/$', bootcamp_auth_views.signup, name='signup'),
+    url(r'^settings/$', core_views.settings, name='settings'),
+    url(r'^settings/picture/$', core_views.picture, name='picture'),
+    url(r'^settings/upload_picture/$', core_views.upload_picture,
+        name='upload_picture'),
+    url(r'^settings/save_uploaded_picture/$', core_views.save_uploaded_picture,
+        name='save_uploaded_picture'),
+    url(r'^settings/password/$', core_views.password, name='password'),
+    url(r'^network/$', core_views.network, name='network'),
+    url(r'^feeds/', include('eblogger.feeds.urls')),
+    url(r'^questions/', include('eblogger.questions.urls')),
+    url(r'^articles/', include('eblogger.articles.urls')),
+    url(r'^messages/', include('eblogger.messenger.urls')),
+    url(r'^notifications/$', activities_views.notifications,
+        name='notifications'),
+    url(r'^notifications/last/$', activities_views.last_notifications,
+        name='last_notifications'),
+    url(r'^notifications/check/$', activities_views.check_notifications,
+        name='check_notifications'),
+    url(r'^search/$', search_views.search, name='search'),
+    url(r'^(?P<username>[^/]+)/$', core_views.profile, name='profile'),
+    url(r'^i18n/', include('django.conf.urls.i18n', namespace='i18n')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
